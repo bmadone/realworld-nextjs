8f9e0f285e11b93fdf93f47cb4ac0005dda8672b
Dmitry Moiseev <mdmitry94@gmail.com>
2024-07-20 16:32:06 +0200

    Main page functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,180 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Article, User } from '@/api/types';
+import { useUser } from '@/contexts/UserContext';
+import React from 'react';
+import cookie from 'cookie';
+import clsx from 'clsx';
+import useSWR from 'swr';
+import { getAuthToken } from '@/utils/auth';
 
 interface HomeProps {
-  articles: Article[];
+  initialArticlesData: { articles: Article[]; articlesCount: number };
+  initialFeedData: { articles: Article[]; articlesCount: number };
   tags: string[];
   user?: User;
 }
 
-export default function Home({ articles, tags }: HomeProps) {
+function ArticlesList({ articles }: { articles: Article[] }) {
+  if (!articles.length) {
+    return <div className="article-preview">No articles are here... yet.</div>;
+  }
+  return articles.map((article) => (
+    <div key={article.slug} className="article-preview">
+      <div className="article-meta">
+        <Link href={`/profile/${article.author.username}`}>
+          <Image
+            alt="Avatar"
+            src={article.author.image}
+            width={30}
+            height={26}
+          />
+        </Link>
+        <div className="info">
+          <Link href={`/profile/${article.author.username}`} className="author">
+            {article.author.username}
+          </Link>
+          <span className="date">
+            {new Date(article.createdAt).toDateString()}
+          </span>
+        </div>
+        <button className="btn btn-outline-primary btn-sm pull-xs-right">
+          <i className="ion-heart"></i> {article.favoritesCount}
+        </button>
+      </div>
+      <Link href={`/article/${article.slug}`} className="preview-link">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>
+        <ul className="tag-list">
+          {article.tagList.map((tag) => (
+            <li key={tag} className="tag-default tag-pill tag-outline">
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </Link>
+    </div>
+  ));
+}
+
+function Pagination({
+  currentPage,
+  total,
+  onPageChange,
+}: {
+  currentPage: number;
+  total: number;
+  onPageChange: (page: number) => void;
+}) {
+  if (total <= 10) {
+    return null;
+  }
+  return (
+    <ul className="pagination">
+      {Array.from({ length: Math.ceil(total / 10) }, (_, i) => i + 1).map(
+        (page) => (
+          <li
+            key={page}
+            className={clsx('page-item', {
+              active: page === currentPage,
+            })}
+          >
+            <Link
+              className="page-link"
+              href=""
+              onClick={(e) => {
+                e.preventDefault();
+                onPageChange(page);
+              }}
+            >
+              {page}
+            </Link>
+          </li>
+        )
+      )}
+    </ul>
+  );
+}
+
+function Tags({
+  tags,
+  onTagClick,
+}: {
+  tags: string[];
+  onTagClick: (tag: string) => void;
+}) {
+  return (
+    <div className="sidebar">
+      <p>Popular Tags</p>
+
+      <div className="tag-list">
+        {tags.map((tag) => (
+          <Link
+            key={tag}
+            href=""
+            className="tag-pill tag-default"
+            onClick={() => onTagClick(tag)}
+          >
+            {tag}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function buildUrl(baseUrl: string, params: Record<string, any>) {
+  const url = new URL(baseUrl);
+  Object.keys(params).forEach((key) => {
+    if (params[key] !== undefined) {
+      url.searchParams.append(key, params[key]);
+    }
+  });
+  return url.toString();
+}
+
+export default function Home({
+  initialArticlesData,
+  initialFeedData,
+  tags,
+}: HomeProps) {
+  const { user } = useUser();
+  const [currentFeed, setCurrentFeed] = React.useState<
+    'global' | 'personal' | 'tag'
+  >('global');
+  const [currentTag, setCurrentTag] = React.useState<string | undefined>(
+    undefined
+  );
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const offset = (currentPage - 1) * 10;
+  const isPersonalFeedSelected = currentFeed === 'personal';
+  const url = buildUrl(
+    isPersonalFeedSelected
+      ? 'https://api.realworld.io/api/articles/feed'
+      : 'https://api.realworld.io/api/articles',
+    {
+      limit: 10,
+      offset,
+      tag: currentTag,
+    }
+  );
+
+  const { data, error } = useSWR(
+    url,
+    () =>
+      fetch(url, {
+        headers: {
+          Authorization: `Token ${getAuthToken()}`,
+        },
+      }).then((res) => res.json()),
+    {
+      fallbackData: isPersonalFeedSelected
+        ? initialFeedData
+        : initialArticlesData,
+    }
+  );
+
   return (
     <>
       <Head>
@@ -31,96 +197,74 @@ export default function Home({ articles, tags }: HomeProps) {
               <div className="col-md-9">
                 <div className="feed-toggle">
                   <ul className="nav nav-pills outline-active">
+                    {user ? (
+                      <li className="nav-item">
+                        <Link
+                          className={clsx('nav-link', {
+                            active: currentFeed === 'personal',
+                          })}
+                          href=""
+                          onClick={(e) => {
+                            setCurrentFeed('personal');
+                            setCurrentTag(undefined);
+                          }}
+                        >
+                          Your Feed
+                        </Link>
+                      </li>
+                    ) : null}
                     <li className="nav-item">
-                      <Link className="nav-link" href="">
-                        Your Feed
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link active" href="">
+                      <Link
+                        className={clsx('nav-link', {
+                          active: currentFeed === 'global',
+                        })}
+                        href=""
+                        onClick={(e) => {
+                          setCurrentFeed('global');
+                          setCurrentTag(undefined);
+                        }}
+                      >
                         Global Feed
                       </Link>
                     </li>
+                    {currentTag ? (
+                      <li className="nav-item">
+                        <Link
+                          href=""
+                          className={clsx('nav-link', {
+                            active: currentFeed === 'tag',
+                          })}
+                        >
+                          <i className="ion-pound"></i> {currentTag}
+                        </Link>
+                      </li>
+                    ) : null}
                   </ul>
                 </div>
 
-                {articles.map((article) => (
-                  <div key={article.slug} className="article-preview">
-                    <div className="article-meta">
-                      <Link href={`/profile/${article.author.username}`}>
-                        <Image
-                          alt="Avatar"
-                          src={article.author.image}
-                          width={30}
-                          height={26}
-                        />
-                      </Link>
-                      <div className="info">
-                        <Link
-                          href={`/profile/${article.author.username}`}
-                          className="author"
-                        >
-                          {article.author.username}
-                        </Link>
-                        <span className="date">
-                          {new Date(article.createdAt).toDateString()}
-                        </span>
-                      </div>
-                      <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                        <i className="ion-heart"></i> {article.favoritesCount}
-                      </button>
-                    </div>
-                    <Link
-                      href={`/article/${article.slug}`}
-                      className="preview-link"
-                    >
-                      <h1>{article.title}</h1>
-                      <p>{article.description}</p>
-                      <span>Read more...</span>
-                      <ul className="tag-list">
-                        {article.tagList.map((tag) => (
-                          <li
-                            key={tag}
-                            className="tag-default tag-pill tag-outline"
-                          >
-                            {tag}
-                          </li>
-                        ))}
-                      </ul>
-                    </Link>
-                  </div>
-                ))}
+                {error ? (
+                  <div>Error loading articles</div>
+                ) : (
+                  <ArticlesList articles={data.articles} />
+                )}
 
-                <ul className="pagination">
-                  <li className="page-item active">
-                    <Link className="page-link" href="">
-                      1
-                    </Link>
-                  </li>
-                  <li className="page-item">
-                    <Link className="page-link" href="">
-                      2
-                    </Link>
-                  </li>
-                </ul>
+                <Pagination
+                  currentPage={currentPage}
+                  total={data.articlesCount}
+                  onPageChange={(page) => {
+                    setCurrentPage(page);
+                  }}
+                />
               </div>
 
               <div className="col-md-3">
-                <div className="sidebar">
-                  <p>Popular Tags</p>
-
-                  <div className="tag-list">
-                    {tags.map((tag) => (
-                      <Link
-                        key={tag}
-                        href={`/tag/${tag}`}
-                        className="tag-pill tag-default"
-                      >
-                        {tag}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
+                <Tags
+                  tags={tags}
+                  onTagClick={(tag) => {
+                    setCurrentTag(tag);
+                    setCurrentFeed('tag');
+                  }}
+                />
               </div>
             </div>
           </div>
@@ -130,23 +274,33 @@ export default function Home({ articles, tags }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const token = cookies.auth_token || null;
+
   try {
-    const [articlesRes, tagsRes] = await Promise.all([
+    const [articlesRes, feedRes, tagsRes] = await Promise.all([
       fetch('https://api.realworld.io/api/articles?limit=10&offset=0'),
+      fetch('https://api.realworld.io/api/articles/feed?limit=10&offset=0', {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }),
       fetch('https://api.realworld.io/api/tags'),
     ]);
 
-    if (!articlesRes.ok || !tagsRes.ok) {
+    if (!articlesRes.ok || !feedRes.ok || !tagsRes.ok) {
       throw new Error('Failed to fetch data');
     }
 
-    const articlesData = await articlesRes.json();
+    const initialArticlesData = await articlesRes.json();
+    const initialFeedData = await feedRes.json();
     const tagsData = await tagsRes.json();
 
     return {
       props: {
-        articles: articlesData.articles,
+        initialArticlesData,
+        initialFeedData,
         tags: tagsData.tags,
       },
     };
@@ -154,7 +308,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
     console.error('Error fetching data:', error);
     return {
       props: {
-        articles: [],
+        initialArticlesData: { articles: [], articlesCount: 0 },
+        initialFeedData: { articles: [], articlesCount: 0 },
         tags: [],
       },
     };
