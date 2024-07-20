ef2d7725c347031436e8be45021f7a6807cd5248
Dmitry Moiseev <mdmitry94@gmail.com>
2024-07-20 17:24:17 +0200

    Settings page

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,7 +1,74 @@
+import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { getAuthToken } from '@/utils/auth';
+import { Article, User } from '@/api/types';
+import cookie from 'cookie';
+import { useUser } from '@/contexts/UserContext';
+import React from 'react';
 
-export default function Profile() {
+interface ProfileProps {
+  initialProfile: User;
+  initialArticles: Article[];
+  initialFavoritedArticles: Article[];
+}
+
+const fetcher = (url: string) =>
+  fetch(url, {
+    headers: {
+      Authorization: `Token ${getAuthToken()}`,
+    },
+  }).then((res) => res.json());
+
+export default function Profile({
+  initialProfile,
+  initialArticles,
+  initialFavoritedArticles,
+}: ProfileProps) {
+  const router = useRouter();
+  const user = useUser();
+  const { username } = router.query;
+
+  const { data: profileData, error: profileError } = useSWR(
+    username ? `https://api.realworld.io/api/profiles/${username}` : null,
+    fetcher,
+    { fallbackData: { profile: initialProfile } }
+  );
+
+  const { data: articlesData, error: articlesError } = useSWR(
+    username
+      ? `https://api.realworld.io/api/articles?author=${username}&limit=10&offset=0`
+      : null,
+    fetcher,
+    { fallbackData: { articles: initialArticles } }
+  );
+
+  const { data: favoritedArticlesData, error: favoritedArticlesError } = useSWR(
+    username
+      ? `https://api.realworld.io/api/articles?favorited=${username}&limit=10&offset=0`
+      : null,
+    fetcher,
+    { fallbackData: { articles: initialFavoritedArticles } }
+  );
+
+  const profile = profileData.profile;
+  const articles = articlesData.articles || [];
+  const favoritedArticles = favoritedArticlesData.articles || [];
+
+  const [currentView, setCurrentView] = React.useState<'my' | 'favourite'>(
+    'my'
+  );
+
+  if (profileError || articlesError || favoritedArticlesError) {
+    return <div>Error loading data</div>;
+  }
+
+  if (!profileData || !articlesData || !favoritedArticlesData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="profile-page">
       <div className="user-info">
@@ -10,25 +77,26 @@ export default function Profile() {
             <div className="col-xs-12 col-md-10 offset-md-1">
               <Image
                 alt="Avatar"
-                src="http://i.imgur.com/Qr71crq.jpg"
+                src={profile.image}
                 className="user-img"
                 width={100}
                 height={100}
                 style={{ display: 'initial' }}
               />
-              <h4>Eric Simons</h4>
-              <p>
-                Cofounder @GoThinkster, lived in Aol&apos;s HQ for a few months,
-                kinda looks like Peeta from the Hunger Games
-              </p>
-              <button className="btn btn-sm btn-outline-secondary action-btn">
-                <i className="ion-plus-round"></i>
-                &nbsp; Follow Eric Simons
-              </button>
-              <button className="btn btn-sm btn-outline-secondary action-btn">
-                <i className="ion-gear-a"></i>
-                &nbsp; Edit Profile Settings
-              </button>
+              <h4>{profile.username}</h4>
+              <p>{profile.bio}</p>
+
+              {profile.username === user.user?.username ? (
+                <button className="btn btn-sm btn-outline-secondary action-btn">
+                  <i className="ion-gear-a"></i>
+                  &nbsp; Edit Profile Settings
+                </button>
+              ) : (
+                <button className="btn btn-sm btn-outline-secondary action-btn">
+                  <i className="ion-plus-round"></i>
+                  &nbsp; Follow {profile.username}
+                </button>
+              )}
             </div>
           </div>
         </div>
@@ -40,91 +108,132 @@ export default function Profile() {
             <div className="articles-toggle">
               <ul className="nav nav-pills outline-active">
                 <li className="nav-item">
-                  <Link className="nav-link active" href="">
+                  <Link
+                    className={`nav-link ${currentView === 'my' ? 'active' : ''}`}
+                    href=""
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentView('my');
+                    }}
+                  >
                     My Articles
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" href="">
+                  <Link
+                    className={`nav-link ${currentView === 'favourite' ? 'active' : ''}`}
+                    href=""
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentView('favourite');
+                    }}
+                  >
                     Favorited Articles
                   </Link>
                 </li>
               </ul>
             </div>
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <Link href="/profile/eric-simons">
-                  <Image
-                    width={50}
-                    height={50}
-                    alt="Alt"
-                    src="http://i.imgur.com/Qr71crq.jpg"
-                  />
-                </Link>
-                <div className="info">
-                  <Link href="/profile/eric-simons" className="author">
-                    Eric Simons
-                  </Link>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 29
-                </button>
-              </div>
-              <Link
-                href="/article/how-to-buil-webapps-that-scale"
-                className="preview-link"
-              >
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-                <ul className="tag-list">
-                  <li className="tag-default tag-pill tag-outline">
-                    realworld
-                  </li>
-                  <li className="tag-default tag-pill tag-outline">
-                    implementations
-                  </li>
-                </ul>
-              </Link>
-            </div>
+            {currentView === 'my'
+              ? articles.map((article) => (
+                  <div key={article.slug} className="article-preview">
+                    <div className="article-meta">
+                      <Link href={`/profile/${article.author.username}`}>
+                        <Image
+                          width={50}
+                          height={50}
+                          alt="Alt"
+                          src={article.author.image}
+                        />
+                      </Link>
+                      <div className="info">
+                        <Link
+                          href={`/profile/${article.author.username}`}
+                          className="author"
+                        >
+                          {article.author.username}
+                        </Link>
+                        <span className="date">
+                          {new Date(article.createdAt).toDateString()}
+                        </span>
+                      </div>
+                      <button className="btn btn-outline-primary btn-sm pull-xs-right">
+                        <i className="ion-heart"></i> {article.favoritesCount}
+                      </button>
+                    </div>
+                    <Link
+                      href={`/article/${article.slug}`}
+                      className="preview-link"
+                    >
+                      <h1>{article.title}</h1>
+                      <p>{article.description}</p>
+                      <span>Read more...</span>
+                      <ul className="tag-list">
+                        {article.tagList.map((tag) => (
+                          <li
+                            key={tag}
+                            className="tag-default tag-pill tag-outline"
+                          >
+                            {tag}
+                          </li>
+                        ))}
+                      </ul>
+                    </Link>
+                  </div>
+                ))
+              : null}
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <Link href="/profile/albert-pai">
-                  <Image
-                    width={50}
-                    height={50}
-                    alt="Alt"
-                    src="http://i.imgur.com/N4VcUeJ.jpg"
-                  />
-                </Link>
-                <div className="info">
-                  <Link href="/profile/albert-pai" className="author">
-                    Albert Pai
-                  </Link>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 32
-                </button>
-              </div>
-              <Link href="/article/the-song-you" className="preview-link">
-                <h1>
-                  The song you won&apos;t ever stop singing. No matter how hard
-                  you try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-                <ul className="tag-list">
-                  <li className="tag-default tag-pill tag-outline">Music</li>
-                  <li className="tag-default tag-pill tag-outline">Song</li>
-                </ul>
-              </Link>
-            </div>
+            {currentView === 'favourite'
+              ? favoritedArticles.map((article) => (
+                  <div key={article.slug} className="article-preview">
+                    <div className="article-meta">
+                      <Link href={`/profile/${article.author.username}`}>
+                        <Image
+                          width={50}
+                          height={50}
+                          alt="Alt"
+                          src={article.author.image}
+                        />
+                      </Link>
+                      <div className="info">
+                        <Link
+                          href={`/profile/${article.author.username}`}
+                          className="author"
+                        >
+                          {article.author.username}
+                        </Link>
+                        <span className="date">
+                          {new Date(article.createdAt).toDateString()}
+                        </span>
+                      </div>
+                      <button className="btn btn-outline-primary btn-sm pull-xs-right">
+                        <i className="ion-heart"></i> {article.favoritesCount}
+                      </button>
+                    </div>
+                    <Link
+                      href={`/article/${article.slug}`}
+                      className="preview-link"
+                    >
+                      <h1>{article.title}</h1>
+                      <p>{article.description}</p>
+                      <span>Read more...</span>
+                      <ul className="tag-list">
+                        {article.tagList.map((tag) => (
+                          <li
+                            key={tag}
+                            className="tag-default tag-pill tag-outline"
+                          >
+                            {tag}
+                          </li>
+                        ))}
+                      </ul>
+                    </Link>
+                  </div>
+                ))
+              : null}
 
-            <ul className="pagination">
+            {/* Implement pagination */}
+            {/* <ul className="pagination">
               <li className="page-item active">
                 <Link className="page-link" href="">
                   1
@@ -135,10 +244,84 @@ export default function Profile() {
                   2
                 </Link>
               </li>
-            </ul>
+            </ul> */}
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  req,
+}) => {
+  const { username } = params as { username: string };
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const token = cookies.auth_token || null;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const [profileRes, articlesRes, favoritedArticlesRes] = await Promise.all([
+      fetch(`https://api.realworld.io/api/profiles/${username}`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }),
+      fetch(
+        `https://api.realworld.io/api/articles?author=${username}&limit=10&offset=0`,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      ),
+      fetch(
+        `https://api.realworld.io/api/articles?favorited=${username}&limit=10&offset=0`,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      ),
+    ]);
+
+    if (!profileRes.ok || !articlesRes.ok || !favoritedArticlesRes.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const { profile } = await profileRes.json();
+    const { articles } = await articlesRes.json();
+    const { articles: favoritedArticles } = await favoritedArticlesRes.json();
+
+    return {
+      props: {
+        initialProfile: profile,
+        initialArticles: articles,
+        initialFavoritedArticles: favoritedArticles,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        initialProfile: {
+          username: '',
+          bio: '',
+          image: '',
+          following: false,
+        },
+        initialArticles: [],
+        initialFavoritedArticles: [],
+      },
+    };
+  }
+};
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,4 +1,125 @@
-export default function Settings() {
+import React from 'react';
+import Link from 'next/link';
+import useSWR from 'swr';
+import { getAuthToken, setAuthToken } from '@/utils/auth';
+import { Article, User } from '@/api/types';
+import { useRouter } from 'next/router';
+
+interface Profile {
+  username: string;
+  bio: string | null;
+  image: string;
+  following: boolean;
+}
+
+interface SettingsProps {
+  initialProfile: Profile;
+  initialArticles: Article[];
+  initialFavoritedArticles: Article[];
+}
+
+const fetcher = (url: string) =>
+  fetch(url, {
+    headers: {
+      Authorization: `Token ${getAuthToken()}`,
+    },
+  }).then((res) => res.json());
+
+export default function Settings({
+  initialProfile,
+  initialArticles,
+  initialFavoritedArticles,
+}: SettingsProps) {
+  const router = useRouter();
+
+  const { data: profileData, error: profileError } = useSWR(
+    'https://api.realworld.io/api/profiles/bmarvinb',
+    fetcher,
+    { fallbackData: { profile: initialProfile } }
+  );
+
+  const { data: articlesData, error: articlesError } = useSWR(
+    'https://api.realworld.io/api/articles?author=bmarvinb&limit=10&offset=0',
+    fetcher,
+    { fallbackData: { articles: initialArticles } }
+  );
+
+  const { data: favoritedArticlesData, error: favoritedArticlesError } = useSWR(
+    'https://api.realworld.io/api/articles?favorited=bmarvinb&limit=10&offset=0',
+    fetcher,
+    { fallbackData: { articles: initialFavoritedArticles } }
+  );
+
+  const profile = profileData?.profile;
+  const articles = articlesData?.articles || [];
+  const favoritedArticles = favoritedArticlesData?.articles || [];
+
+  const [formData, setFormData] = React.useState({
+    image: profile.image,
+    username: profile.username,
+    bio: profile.bio || '',
+    email: '', // Assuming email is fetched from a different endpoint or context
+    password: '',
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const token = getAuthToken();
+
+    if (!token) {
+      router.push('/login');
+      return;
+    }
+
+    try {
+      const res = await fetch('https://api.realworld.io/api/user', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify({ user: formData }),
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to update profile');
+      }
+
+      const { user } = await res.json();
+      setAuthToken(user.token); // Update the token if it has changed
+      // Optionally, update the local state with the new user data
+      setFormData({
+        image: user.image,
+        username: user.username,
+        bio: user.bio || '',
+        email: user.email,
+        password: '', // Clear the password field
+      });
+
+      // Optionally, show a success message or redirect the user
+      alert('Profile updated successfully');
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      // Optionally, show an error message
+      alert('Failed to update profile');
+    }
+  };
+
+  if (profileError || articlesError || favoritedArticlesError) {
+    return <div>Error loading data</div>;
+  }
+
+  if (!profileData || !articlesData || !favoritedArticlesData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="settings-page">
       <div className="container page">
@@ -10,13 +131,16 @@ export default function Settings() {
               <li>That name is required</li>
             </ul>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
                   <input
                     className="form-control"
                     type="text"
                     placeholder="URL of profile picture"
+                    name="image"
+                    value={formData.image}
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -24,6 +148,9 @@ export default function Settings() {
                     className="form-control form-control-lg"
                     type="text"
                     placeholder="Your Name"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -31,6 +158,9 @@ export default function Settings() {
                     className="form-control form-control-lg"
                     rows={8}
                     placeholder="Short bio about you"
+                    name="bio"
+                    value={formData.bio}
+                    onChange={handleChange}
                   ></textarea>
                 </fieldset>
                 <fieldset className="form-group">
@@ -38,6 +168,9 @@ export default function Settings() {
                     className="form-control form-control-lg"
                     type="text"
                     placeholder="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -45,6 +178,9 @@ export default function Settings() {
                     className="form-control form-control-lg"
                     type="password"
                     placeholder="New Password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <button className="btn btn-lg btn-primary pull-xs-right">
@@ -58,7 +194,161 @@ export default function Settings() {
             </button>
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="text-xs-center">Your Articles</h2>
+            {articles.length === 0 ? (
+              <p>No articles are here... yet.</p>
+            ) : (
+              <ul className="article-list">
+                {articles.map((article) => (
+                  <li key={article.slug} className="article-preview">
+                    <div className="article-meta">
+                      <Link href={`/profile/${article.author.username}`}>
+                        <img
+                          src={article.author.image}
+                          alt={article.author.username}
+                        />
+                      </Link>
+                      <div className="info">
+                        <Link
+                          href={`/profile/${article.author.username}`}
+                          className="author"
+                        >
+                          {article.author.username}
+                        </Link>
+                        <span className="date">
+                          {new Date(article.createdAt).toDateString()}
+                        </span>
+                      </div>
+                    </div>
+                    <Link
+                      href={`/article/${article.slug}`}
+                      className="preview-link"
+                    >
+                      <h1>{article.title}</h1>
+                      <p>{article.description}</p>
+                      <span>Read more...</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="text-xs-center">Favorited Articles</h2>
+            {favoritedArticles.length === 0 ? (
+              <p>No articles are here... yet.</p>
+            ) : (
+              <ul className="article-list">
+                {favoritedArticles.map((article) => (
+                  <li key={article.slug} className="article-preview">
+                    <div className="article-meta">
+                      <Link href={`/profile/${article.author.username}`}>
+                        <img
+                          src={article.author.image}
+                          alt={article.author.username}
+                        />
+                      </Link>
+                      <div className="info">
+                        <Link
+                          href={`/profile/${article.author.username}`}
+                          className="author"
+                        >
+                          {article.author.username}
+                        </Link>
+                        <span className="date">
+                          {new Date(article.createdAt).toDateString()}
+                        </span>
+                      </div>
+                    </div>
+                    <Link
+                      href={`/article/${article.slug}`}
+                      className="preview-link"
+                    >
+                      <h1>{article.title}</h1>
+                      <p>{article.description}</p>
+                      <span>Read more...</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const token = cookies.auth_token || null;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const [profileRes, articlesRes, favoritedArticlesRes] = await Promise.all([
+      fetch('https://api.realworld.io/api/profiles/bmarvinb', {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      }),
+      fetch(
+        'https://api.realworld.io/api/articles?author=bmarvinb&limit=10&offset=0',
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      ),
+      fetch(
+        'https://api.realworld.io/api/articles?favorited=bmarvinb&limit=10&offset=0',
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      ),
+    ]);
+
+    if (!profileRes.ok || !articlesRes.ok || !favoritedArticlesRes.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const { profile } = await profileRes.json();
+    const { articles } = await articlesRes.json();
+    const { articles: favoritedArticles } = await favoritedArticlesRes.json();
+
+    return {
+      props: {
+        initialProfile: profile,
+        initialArticles: articles,
+        initialFavoritedArticles: favoritedArticles,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        initialProfile: {
+          username: '',
+          bio: '',
+          image: '',
+          following: false,
+        },
+        initialArticles: [],
+        initialFavoritedArticles: [],
+      },
+    };
+  }
+};
